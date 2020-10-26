42cb8f8d7bc316d99214f208feb9ba5b348d9f4e
Cíndia Moraes da Silva <cindiams@hotmail.com>
2020-10-26 15:15:13 -0300

    Base do Projeto

---
+++ Principal.ts
@@ -0,0 +1,10 @@
+import prompt from "prompt-sync";
+import { Personagem } from "./Personagem";
+
+let vetorPersonagens: Personagem[] = [];
+
+let teclado = prompt();
+
+let option: number = 0;
+
+//....
